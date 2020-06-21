1e1e700704df2a30a32e48d48981a4bbe96bc7f1
unknown <mstiff1983@gmail.com>
2020-06-21 15:25:19 -0400

    The employee and customer models have been established and the properties on the customer model have been implemented.

---
+++ TrashCollectorProj/Models/Customer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollectorProj.Models
+{
+    public class Customer
+    {
+        [Key]
+
+        public int ID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string StreetAddress { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public int ZipCode { get; set; }
+
+        public DayOfWeek PickUpDay { get; set; }
+
+        public DateTime ExtraPickUp { get; set; }
+
+        public double BalanceOwed { get; set; }
+
+        public DateTime SupendPickUpStart { get; set; }
+
+        public DateTime SuspendPickUpEnd { get; set; }
+
+    }
+}
