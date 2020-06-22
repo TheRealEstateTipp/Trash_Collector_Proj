bce628a9c80ccc7094dda40900f32f5548dd31a5
unknown <mstiff1983@gmail.com>
2020-06-21 20:51:07 -0400

    The Action Filter has been globally set up bu making changes to the configureservices method on the Startup class so that when the action method is executed the onaction method will run before the action method

--- TrashCollectorProj/Startup.cs
+++ TrashCollectorProj/Startup.cs
@@ -12,6 +12,9 @@ using TrashCollectorProj.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using TrashCollectorProj.ActionFilters;
 
 namespace TrashCollectorProj
 {
@@ -32,6 +35,9 @@ namespace TrashCollectorProj
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultUI().AddDefaultTokenProviders();
+
+            services.AddScoped<ClaimsPrincipal>(s => s.GetService<IHttpContextAccessor>().HttpContext.User);
+            services.AddControllers(config => { config.Filters.Add(typeof(GlobalRouting)); });
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
